9ac89da3d17ec753d1a59ee4826688c4375040d5
RobertoZeppilli <robzep171@gmail.com>
2021-10-31 20:35:45 +0100

    react-stripe / details

--- src/stripe/components/Sidebar.js
+++ src/stripe/components/Sidebar.js
@@ -9,28 +9,31 @@ const Sidebar = () => {
 
     return (
         <div className="sidebar">
+            <div>
+                <div className="sidebar-header">
+                    <img src={logo} alt="logo" />
+                    <button onClick={closeSidebar} type="button" className="close-btn">
+                        <FaTimes />
+                    </button>
+                </div>
 
-            <div className="sidebar-header">
-                <img src={logo} alt="logo" />
-                <button onClick={closeSidebar} type="button" className="close-btn">
-                    <FaTimes />
-                </button>
+                <div className="sidebar-links">
+                    {sublinks.map(link => {
+                        return link.links.map(item => {
+                            return (
+                                <div className="sidebar-link" key={item.url}>
+                                    <span>{item.icon}</span>
+                                    <a href={item.url}>{item.label}</a>
+                                </div>
+                            )
+                        })
+                    })}
+
+                </div>
+                <button className="btn sidebar-btn" type="button">sign in</button>
             </div>
 
-            <div className="sidebar-links">
-                {sublinks.map(link => {
-                    return link.links.map(item => {
-                        return (
-                            <div className="sidebar-link" key={item.url}>
-                                <span>{item.icon}</span>
-                                <a href={item.url}>{item.label}</a>
-                            </div>
-                        )
-                    })
-                })}
-
-            </div>
-            <button className="btn sidebar-btn" type="button">sign in</button>
+            <p className="copyrights">&copy; all rights reserved, Stripe 2021</p>
         </div>
     )
 }
